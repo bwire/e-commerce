cfccbdcc942b0297e9deca64e5f0bedf5d6404d3
barbedwire <e.barbedwire@gmail.com>
2023-05-12 15:56:42 +0200

    setup router

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import About from './AboutPage';
+import Cart from './AuthWrapper';
+import Checkout from './CheckoutPage';
+import Error from './ErrorPage';
+import Home from './HomePage';
+import Private from './PrivateRoute';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+
+export { About, Cart, Checkout, Error, Home, Private, Products, SingleProduct };
