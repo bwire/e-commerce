4d1161a2a9ed53967ff474d6398f6f3d95f0a9c2
barbedwire <e.barbedwire@gmail.com>
2023-05-20 13:48:10 +0200

    add checkoutPage draft

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,35 +2,32 @@ import React from 'react';
 import styled from 'styled-components';
 import { PageHero } from '../components';
 import aboutImg from '../assets/hero-bcg.jpeg';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 const AboutPage = () => {
   return (
     <main>
       <PageHero title='about' />
-      <div class='section-center'>
-        <Wrapper className='page section section-center'>
-          <img src={aboutImg} alt='nice desk' />
-          <article>
-            <div class='title'>
-              <h2>our story</h2>
-              <div class='underline'></div>
-            </div>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
-              accusantium sapiente tempora sed dolore esse deserunt eaque
-              excepturi, delectus error accusamus vel eligendi, omnis beatae.
-              Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque
-              dolore, obcaecati incidunt sequi blanditiis est exercitationem
-              molestiae delectus saepe odio eligendi modi porro eaque in libero
-              minus unde sapiente consectetur architecto. Ullam rerum, nemo iste
-              ex, eaque perspiciatis nisi, eum totam velit saepe sed quos
-              similique amet. Ex, voluptate accusamus nesciunt totam vitae esse
-              iste.
-            </p>
-          </article>
-        </Wrapper>
-      </div>
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div class='title'>
+            <h2>our story</h2>
+            <div class='underline'></div>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
+            accusantium sapiente tempora sed dolore esse deserunt eaque
+            excepturi, delectus error accusamus vel eligendi, omnis beatae.
+            Quisquam, dicta. Eos quod quisquam esse recusandae vitae neque
+            dolore, obcaecati incidunt sequi blanditiis est exercitationem
+            molestiae delectus saepe odio eligendi modi porro eaque in libero
+            minus unde sapiente consectetur architecto. Ullam rerum, nemo iste
+            ex, eaque perspiciatis nisi, eum totam velit saepe sed quos
+            similique amet. Ex, voluptate accusamus nesciunt totam vitae esse
+            iste.
+          </p>
+        </article>
+      </Wrapper>
     </main>
   );
 };
