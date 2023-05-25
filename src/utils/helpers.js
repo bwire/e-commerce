f31213956919707cbbbde6cfd49b87819a5cbf1e
barbedwire <e.barbedwire@gmail.com>
2023-05-25 16:30:19 +0200

    add formatPrice function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (price) => {
+  const formatter = Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  return formatter.format(price / 100);
+};
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {};
